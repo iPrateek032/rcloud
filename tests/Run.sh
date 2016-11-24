db548660c81defbec20a9da18f4ba198cf0881e1
iPrateek032 <naik.prateek@outlook.com>
2016-11-24 16:51:32 +0530

    running all the scripts of advanced drop down menu

--- tests/Run.sh
+++ tests/Run.sh
@@ -18,7 +18,8 @@ cd /home/travis/build/iPrateek032/rcloud/tests
 #sudo apt-get install xvfb
 pwd
 echo "Executing testscripts from $1"
-sudo xvfb-run -a casperjs test --ssl-protocol=any --engine=slimerjs $1 --username=RCloudatt --password=musigma12 --url=http://127.0.0.1:8080/login.R --xunit=Reports/report.xml
+# sudo xvfb-run -a casperjs test --ssl-protocol=any --engine=slimerjs $1 --username=RCloudatt --password=musigma12 --url=http://127.0.0.1:8080/login.R --xunit=Reports/report.xml
+sudo xvfb-run -a casperjs test --ssl-protocol=any --engine=slimerjs advanced_dropdown_option/*.js --username=RCloudatt --password=musigma12 --url=http://127.0.0.1:8080/login.R --xunit=Reports/report.xml
 
 echo -e "Starting to update AUTO_IMG\n"
 # change directory to Travis Home
@@ -65,4 +66,37 @@ exit 1
 fi
 
 
+# #!/usr/bin/bash
 
+# sudo libreoffice --headless --convert-to csv Details.ods --outdir 
+
+# INPUT_FILE='Details.csv'
+# i=0
+# IFS=','
+# while read U P T C
+# do 
+# U1[$i]=$U
+# P1[$i]=$P
+# T1[$i]=$T
+# Choice[$i]=$C
+# i=$((i+1))
+# done <$INPUT_FILE
+
+# echo "username: $U1"
+# echo "password: $P1"
+# counter=0
+# j=0
+# while [ $j -lt $i ]
+# do
+# if [ ${Choice[j]} == "TRUE" ]
+# then
+# 	echo "Current Testsuite: ${T1[j]}"
+# 	sudo casperjs test --engine=slimerjs ${T1[j]}/*.js --username=$U1 --password=$P1 --url=http://127.0.0.1:8080/login.R --xunit=Reports/${T1[j]}.xml
+# 	counter=$((counter+1))
+# fi
+# j=$((j+1));
+# done
+# if [ $counter == 0 ] 
+# then
+# 	echo "No Testsuite selected....!!!! Please Select the testsuites from Details1.ods file...!!!! "
+# fi
