825033f1a55740dc5fc77ef8b4b2092cf1d502db
iPrateek032 <naik.prateek@outlook.com>
2016-11-28 16:08:45 +0530

    removed some of the test scripts from 2-3Test folder

--- tests/flex.js
+++ tests/flex.js
@@ -1,17 +1,17 @@
 /*
  Author: Prateek
- Description:This is a casperjs automation script for checking that the published mini.html notebook is visible to the anonymous user
+ Description:This is a casperjs automation script for checking that the published flexdashboard.html notebook is visible to the i user
  */
 
-casper.test.begin("Mini.html notebook opening as a Anonymous user", 4, function suite(test) {
+casper.test.begin("Opening notebook in flexdashboard.html", 4, function suite(test) {
 
     var x = require('casper').selectXPath;
     var github_username = casper.cli.options.username;
     var github_password = casper.cli.options.password;
     var rcloud_url = casper.cli.options.url;
     var functions = require(fs.absolute('basicfunctions.js'));
-    var notebook_id, before_url;
-
+    var notebook_id, URL;
+    
     casper.start(rcloud_url, function () {
         functions.inject_jquery(casper);
     });
@@ -30,7 +30,6 @@ casper.test.begin("Mini.html notebook opening as a Anonymous user", 4, function 
         functions.validation(casper);
     });
 
-    //create a new notebook
     functions.create_notebook(casper);
 
     //Pulling the mini notebook of another user's
@@ -46,7 +45,7 @@ casper.test.begin("Mini.html notebook opening as a Anonymous user", 4, function 
     //entering url
     casper.then(function(){
         casper.evaluate(function () {
-            $('#pull-notebook-url').val("http://127.0.0.1:8080/edit.html?notebook=04233fe343df889fe40574a62d8c1a12");
+            $('#pull-notebook-url').val("http://127.0.0.1:8080/edit.html?notebook=8ddca2abe39c27f6beee375ede5cd83f");
         });      
         console.log("Clicking on pull button");
         this.waitForSelector("div.modal-footer:nth-child(7) > span:nth-child(2)").thenClick("div.modal-footer:nth-child(7) > span:nth-child(2)");
@@ -63,13 +62,8 @@ casper.test.begin("Mini.html notebook opening as a Anonymous user", 4, function 
         this.reload();
         this.wait(12000);
     });
-    functions.runall(casper);
 
-    // casper.wait(10000).then(function (){
-    //     functions.create_notebook(casper);
-    //     functions.addnewcell(casper);
-    //     functions.addcontentstocell(casper, "rcloud.publish.notebook('"+notebook_id+"')");
-    // })
+    // functions.runall(casper);
 
     //publishing the notebook
     casper.then(function (){
@@ -80,25 +74,15 @@ casper.test.begin("Mini.html notebook opening as a Anonymous user", 4, function 
     });
 
 
-    //logging out of RCloud
-    casper.viewport(1366, 768).then(function () {
-        console.log('Logging out of RCloud');
-        this.click("#rcloud-navbar-menu > li:nth-child(7) > a:nth-child(1)");
-        this.wait(6000);
-    });
-
-    //Opening in mini.html as anonymous user
-    casper.then(function (){
-        casper.page = casper.newPage();
-        casper.viewport(1024, 768).open("http://127.0.0.1:8080/mini.html?notebook=" + notebook_id).then(function () {
-            this.wait(10000);
-            this.echo('url loaded successfully now checking for the element');
-            this.wait(10000);
-            this.then(function () {
-                this.test.assertExists("#SWvL_0_0 > div:nth-child(1) > strong:nth-child(1)", 'Required element found hence "Mini.html" notebook opened successfully');
-                this.wait(2000);
-            });
-        });
+    //Accessing Shiny.html for published notebook as anonymous user
+    casper.then( function () {
+        this.thenOpen("http://127.0.0.1:8080/shared.R/rcloud.flexdashboard/flexdashboard.html?notebook=" + notebook_id);
+        this.wait(15000);
+        this.echo('url loaded successfully now checking for the element');
+        this.wait(10000);
+        this.waitForSelector(x("html/body/div[1]/div/div[1]/span[2]"), function (){
+            this.test.assertSelectorHasText(x("html/body/div[1]/div/div[1]/span[2]"), "HTML Widgets Showcase ", "Confirmed that flexdashboard page opened");
+        })
     });
 
     casper.run(function () {
