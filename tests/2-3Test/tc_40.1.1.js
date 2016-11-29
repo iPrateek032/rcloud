07a1e7f9132289eea8d610f542c05ec04028ae2c
iPrateek032 <naik.prateek@outlook.com>
2016-11-29 17:23:07 +0530

    changes in smoketest.js

--- tests/2-3Test/tc_40.1.1.js
+++ tests/2-3Test/tc_40.1.1.js
@@ -14,7 +14,7 @@ casper.test.begin("Notebook info icon", 6, function suite(test) {
     var title;
 
     casper.start(rcloud_url, function () {
-        casper.page.injectJs('jquery-1.10.2.js');
+        functions.inject_jquery(casper);
     });
     casper.wait(10000);
 
