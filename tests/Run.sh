2ec224e6b98f8d6e1bc446c039fedcadfd022cb0
iPrateek032 <naik.prateek@outlook.com>
2016-11-30 12:13:48 +0530

    capturing image

--- tests/Run.sh
+++ tests/Run.sh
@@ -26,12 +26,17 @@ echo -e "Starting to update AUTO_IMG\n"
 cd $Home
 
 #Making a new directory in travis home and open it
+echo -e "Creating Images folder\n"
 mkdir Images
 sudo chmod 777 Images
+echo -e "Opening Images floder\n"
 cd Images
+pwd
 
 #copying file/directory from /home/travis/build/iPrateek032/rcloud/tests to the newly created Images directory
+echo -e "copying file/directory from /home/travis/build/iPrateek032/rcloud/tests to the newly created Images directory\n"
 cp -R /home/travis/build/iPrateek032/rcloud/tests/*.png $HOME/Images/
+ls
 
 #go to travis home directory and configure git
 cd $HOME
