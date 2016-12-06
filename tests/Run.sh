61456a5e11a29a8d9193dc0050f45f56be9f68fe
iPrateek032 <naik.prateek@outlook.com>
2016-12-06 16:55:20 +0530

    changed in Run.sh

--- tests/Run.sh
+++ tests/Run.sh
@@ -8,6 +8,8 @@ cd /home/travis/build/iPrateek032/rcloud/services
 sudo sed -i -e '2iROOT=/home/travis/build/iPrateek032/rcloud\' rcloud-sks
 sudo sh rcloud-sks &
 
+sudo apt-get install libssl-dev
+
 Rscript -e 'chooseCRANmirror(ind=81)'
 Rscript -e 'install.packages("XML", repos=c("http://RForge.net", "http://R.research.att.com"), type="source")'
 Rscript -e 'install.packages("rcloud.dcplot", repos="http://rforge.net")'
@@ -15,6 +17,7 @@ Rscript -e 'install.packages("rpython2", repos="http://rforge.net")'
 Rscript -e 'install.packages("xml2", repos=c("http://RForge.net", "http://R.research.att.com"), type="source")'
 Rscript -e 'install.packages("drat", repos="https://cran.rstudio.com")'
 Rscript -e 'install.packages("devtools", repos="http://rforge.net")'
+# Rscript -e 'devtools::install_github("hadley/devtools")'
 Rscript -e 'devtools::install_github("att/rcloud.rmd")'
 Rscript -e 'devtools::install_github("att/rcloud.shiny")'
 Rscript -e 'devtools::install_github("rcloud.htmlwidgets")'
